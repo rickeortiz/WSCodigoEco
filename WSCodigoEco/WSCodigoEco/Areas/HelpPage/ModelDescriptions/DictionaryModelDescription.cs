0b0be7fd2fcce13cb2c3b7c31c8c0c535c81f587
rickeortiz@hotmail.com <rickeortiz@hotmail.com>
2018-11-18 13:53:32 -0200

    Projeto Inicial

---
+++ WSCodigoEco/WSCodigoEco/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WSCodigoEco.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WSCodigoEco/WSCodigoEco/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WSCodigoEco.WebApiApplication" Language="C#" %>
